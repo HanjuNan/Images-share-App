83b74b2ef2e8f27a715241ba3fbc6b24d688c5c5
hanjunan <1401224905@qq.com>
2023-08-09 00:05:11 +0800

    根据开发和生产环境,自动选择baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,27 @@
 import axios from "axios"
+import md5 from 'md5'
 
+
+// console.log(import.meta.env.VITE_BASE_API);
+
+
+
+const service = axios.create({
+    //根据项目的状态,自动切换请求的服务地址
+    baseURL: 'import.meta.env.VITE_BASE_API',
+    timeout: 5000
+})
+
+/** 请求拦截器 */
+const requestHandler = (config) => {
+    const { icode, time } = getIcode()
+    config.headers.icode = icode
+    config.headers.codetype = time
+    return config
+}
+
+service.interceptors.request.use(requestHandler, (error) => {
+return Promise.reject(error)
+})
 
 export default service
