8d4902a840c97bf96b1f4854f7201975b94f85bd
hanjunan <1401224905@qq.com>
2023-08-08 23:44:53 +0800

    获取category

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,28 +1,4 @@
 import axios from "axios"
-import md5 from 'md5'
 
-/** 获取icode */
-const getIcode = () => {
-  const e = parseInt(Date.now() / 1e3),
-    t = e + 'LGD_Sunday-1991-12-30'
-  return { icode: md5(t), time: e }
-}
-
-const service = axios.create({
-    baseURL: '/api',
-    timeout: 5000
-})
-
-/** 请求拦截器 */
-const requestHandler = (config) => {
-    const { icode, time } = getIcode()
-    config.headers.icode = icode
-    config.headers.codetype = time
-    return config
-}
-
-service.interceptors.request.use(requestHandler, (error) => {
-return Promise.reject(error)
-})
 
 export default service
